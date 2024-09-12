3b85f58681dad55fde337e8f98cf2d301999d250
Asmit <140798529+iAsmit19@users.noreply.github.com>
2024-09-12 21:51:01 +0530

    Initialization_0.8

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -1,9 +1,10 @@
 import { nav_logo } from "../../stylesheets/index.module.css";
+import arockLogo from "/src/assets/logos/arock-logo.svg";
 
 const Logo = () => {
   return (
     <div>
-      <img className={nav_logo} src={"/public/arock-logo.svg"} alt="" />
+      <img className={nav_logo} src={arockLogo} alt="" />
     </div>
   );
 };
